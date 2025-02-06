04629b6b66adf7040f18a2a6a98f18948f666bf1
recrumptor <recrumptor@gmail.com>
2025-02-07 01:27:19 +0500

    Update feeds.sh

--- scripts/feeds.sh
+++ scripts/feeds.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Main Feeds
-sed -i '$a src-git small_package https://github.com/kenzok8/small-package' feeds.conf.default
-# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git small_package https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Extra Packages
